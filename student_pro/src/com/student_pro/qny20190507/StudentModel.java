7a7a5b2b6eec07ee86e8e92ef1392b5aa4c3ac97
qinnianyu1998 <916892041@qq.com>
2019-05-07 17:00:50 +0800

    "第一次提交"

---
+++ student_pro/src/com/student_pro/qny20190507/StudentModel.java
@@ -0,0 +1,54 @@
+package com.student_pro.qny20190507;
+
+public class StudentModel {
+
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+	public StudentModel(int id, String username, String password, int sex, String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.love = love;
+	}
+	public StudentModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
